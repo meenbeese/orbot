3c422be4e48bd1ef07f5e96cdd0db890d7fd15c7
Meenbeese <meenbeese@tutanota.com>
2024-07-20 12:58:00 -0400

    nio

--- app/src/main/java/org/torproject/android/v3onionservice/ZipUtilities.kt
+++ app/src/main/java/org/torproject/android/v3onionservice/ZipUtilities.kt
@@ -1,18 +1,17 @@
 package org.torproject.android.v3onionservice
 
+import android.annotation.SuppressLint
 import android.content.ContentResolver
 import android.net.Uri
 
-import java.io.BufferedInputStream
-import java.io.BufferedOutputStream
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
 import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.util.zip.ZipEntry
 import java.util.zip.ZipInputStream
 import java.util.zip.ZipOutputStream
 
+@SuppressLint("NewApi")
 class ZipUtilities(
     private val files: Array<String>?,
     private val zipFile: Uri,
@@ -20,23 +19,16 @@ class ZipUtilities(
 ) {
     fun zip(): Boolean {
         return try {
-            checkNotNull(contentResolver.openFileDescriptor(zipFile, "w")).use { pdf ->
-                FileOutputStream(pdf.fileDescriptor).use { dest ->
-                    ZipOutputStream(BufferedOutputStream(dest)).use { out ->
-                        val data = ByteArray(BUFFER)
-                        checkNotNull(files)
-                        for (file in files) {
-                            FileInputStream(file).use { fi ->
-                                BufferedInputStream(fi, BUFFER).use { origin ->
-                                    val entry = ZipEntry(file.substring(file.lastIndexOf("/") + 1))
-                                    out.putNextEntry(entry)
-                                    var count: Int
-                                    while (origin.read(data, 0, BUFFER).also { count = it } != -1) {
-                                        out.write(data, 0, count)
-                                    }
-                                }
-                            }
-                        }
+            val zipFilePath = Paths.get(zipFile.path!!)
+            Files.newOutputStream(zipFilePath).use { out ->
+                ZipOutputStream(out).use { zipOut ->
+                    checkNotNull(files)
+                    for (filePath in files) {
+                        val file = Paths.get(filePath)
+                        val zipEntry = ZipEntry(file.fileName.toString())
+                        zipOut.putNextEntry(zipEntry)
+                        Files.copy(file, zipOut)
+                        zipOut.closeEntry()
                     }
                 }
             }
@@ -49,66 +41,31 @@ class ZipUtilities(
 
     fun unzip(outputPath: String): Boolean {
         return try {
-            contentResolver.openInputStream(zipFile)?.use { inputStream ->
-                val zis = ZipInputStream(BufferedInputStream(inputStream))
-                extractFromZipInputStream(outputPath, zis)
-            } ?: false
-        } catch (e: IOException) {
-            e.printStackTrace()
-            false
-        }
-    }
-
-    private fun extractFromZipInputStream(outputPath: String, zis: ZipInputStream): Boolean {
-        val outputDir = File(outputPath)
-        return try {
-            var ze: ZipEntry
-            val buffer = ByteArray(1024)
-
-            outputDir.mkdirs()
-
-            while ((zis.nextEntry.also { ze = it }) != null) {
-                val filename = ze.name
-
-                if (!ONION_SERVICE_CONFIG_FILES.contains(filename)) { // *any* kind of foreign file
-                    val writtenFiles = outputDir.listFiles()
-                    if (writtenFiles != null) {
-                        for (writtenFile in writtenFiles) {
-                            writtenFile.delete()
+            val zipFilePath = Paths.get(zipFile.path!!)
+            val outputDir = Paths.get(outputPath)
+            Files.newInputStream(zipFilePath).use { inStream ->
+                ZipInputStream(inStream).use { zipIn ->
+                    var entry = zipIn.nextEntry
+                    while (entry != null) {
+                        val filePath = outputDir.resolve(entry.name)
+                        if (!entry.isDirectory) {
+                            Files.copy(zipIn, filePath)
+                        } else {
+                            Files.createDirectories(filePath)
                         }
-                    }
-                    outputDir.delete()
-                    return false
-                }
-
-                // Need to create directories if not exists, or it will generate an Exception...
-                if (ze.isDirectory) {
-                    val fmd = File("$outputPath/$filename")
-                    fmd.mkdirs()
-                    continue
-                }
-
-                FileOutputStream("$outputPath/$filename").use { fout ->
-                    var count: Int
-                    while (zis.read(buffer).also { count = it } != -1) {
-                        fout.write(buffer, 0, count)
+                        zipIn.closeEntry()
+                        entry = zipIn.nextEntry
                     }
                 }
-
-                zis.closeEntry()
             }
-
             true
         } catch (e: IOException) {
             e.printStackTrace()
             false
-        } finally {
-            zis.close()
         }
     }
 
     companion object {
-        private const val BUFFER = 2048
         const val ZIP_MIME_TYPE: String = "application/zip"
 
         private val ONION_SERVICE_CONFIG_FILES: List<String> = mutableListOf(
