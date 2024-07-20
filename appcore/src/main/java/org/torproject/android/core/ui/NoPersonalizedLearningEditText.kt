faaa4ded02b998b1b1f7843c3dd17757d2915f0e
Meenbeese <meenbeese@tutanota.com>
2024-07-19 20:16:48 -0400

    color fixes

--- appcore/src/main/java/org/torproject/android/core/ui/NoPersonalizedLearningEditText.kt
+++ appcore/src/main/java/org/torproject/android/core/ui/NoPersonalizedLearningEditText.kt
@@ -3,9 +3,13 @@ package org.torproject.android.core.ui
 import android.content.Context
 import android.util.AttributeSet
 import android.view.inputmethod.EditorInfo
-import androidx.appcompat.widget.AppCompatEditText
 
-class NoPersonalizedLearningEditText(context: Context, attrs: AttributeSet?) : AppCompatEditText(context, attrs) {
+import com.google.android.material.textfield.TextInputEditText
+
+class NoPersonalizedLearningEditText(
+    context: Context,
+    attrs: AttributeSet?
+) : TextInputEditText(context, attrs) {
     init {
         imeOptions = imeOptions or EditorInfo.IME_FLAG_NO_PERSONALIZED_LEARNING
     }
