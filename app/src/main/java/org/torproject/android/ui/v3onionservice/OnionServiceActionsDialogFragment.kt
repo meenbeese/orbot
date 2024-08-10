104f0842bee1f5648b3dd3f9bb8b9e86dbd7b9f1
Meenbeese <meenbeese@tutanota.com>
2024-08-10 17:18:59 -0400

    Update OnionServiceActionsDialogFragment.kt

--- app/src/main/java/org/torproject/android/ui/v3onionservice/OnionServiceActionsDialogFragment.kt
+++ app/src/main/java/org/torproject/android/ui/v3onionservice/OnionServiceActionsDialogFragment.kt
@@ -2,7 +2,6 @@ package org.torproject.android.ui.v3onionservice
 
 import android.app.Activity
 import android.app.Dialog
-import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
@@ -19,57 +18,55 @@ import org.torproject.android.R
 import org.torproject.android.core.ClipboardUtils.copyToClipboard
 import org.torproject.android.core.DiskUtils.createWriteFileIntent
 
-class OnionServiceActionsDialogFragment internal constructor(arguments: Bundle?) :
-    DialogFragment() {
+class OnionServiceActionsDialogFragment(arguments: Bundle?) : DialogFragment() {
+
     init {
         setArguments(arguments)
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val arguments = arguments
-        val ad = AlertDialog.Builder(requireActivity())
-            .setItems(
+        val arguments = requireArguments()
+        return AlertDialog.Builder(requireActivity()).apply {
+            setItems(
                 arrayOf(
                     getString(R.string.copy_address_to_clipboard),
                     Html.fromHtml(getString(R.string.backup_service), Html.FROM_HTML_MODE_LEGACY),
                     getString(R.string.delete_service)
                 ), null
             )
-            .setNegativeButton(android.R.string.cancel) { dialog: DialogInterface, _: Int -> dialog.dismiss() }
-            .setTitle(R.string.hidden_services)
-            .create()
-
-        // Done this way so we can startActivityForResult on backup without the dialog vanishing
-        ad.listView.onItemClickListener =
-            AdapterView.OnItemClickListener { _: AdapterView<*>?, _: View?, position: Int, _: Long ->
-                when (position) {
-                    0 -> doCopy(requireArguments(), context)
-                    1 -> doBackup(requireArguments(), context)
-                    2 -> {
-                        OnionServiceDeleteDialogFragment(requireArguments()).show(
-                            parentFragmentManager,
-                            OnionServiceDeleteDialogFragment::class.java.simpleName
-                        )
+            setNegativeButton(android.R.string.cancel) { dialog: DialogInterface, _: Int -> dialog.dismiss() }
+            setTitle(R.string.hidden_services)
+        }.create().apply {
+            listView.onItemClickListener =
+                AdapterView.OnItemClickListener { _: AdapterView<*>?, _: View?, position: Int, _: Long ->
+                    when (position) {
+                        0 -> doCopy(arguments)
+                        1 -> doBackup(arguments)
+                        2 -> {
+                            OnionServiceDeleteDialogFragment(arguments).show(
+                                parentFragmentManager,
+                                OnionServiceDeleteDialogFragment::class.java.simpleName
+                            )
+                        }
                     }
+                    if (position != 1) dismiss()
                 }
-                if (position != 1) dismiss()
-            }
-        return ad
+        }
     }
 
-    private fun doCopy(arguments: Bundle, context: Context?) {
+    private fun doCopy(arguments: Bundle) {
         val onion = arguments.getString(OnionServiceActivity.BUNDLE_KEY_DOMAIN)
         if (onion == null) {
-            Toast.makeText(context, R.string.please_restart_Orbot_to_enable_the_changes, Toast.LENGTH_LONG).show()
+            Toast.makeText(requireContext(), R.string.please_restart_Orbot_to_enable_the_changes, Toast.LENGTH_LONG).show()
         } else {
             copyToClipboard("onion", onion, getString(R.string.done), requireContext())
         }
     }
 
-    private fun doBackup(arguments: Bundle, context: Context?) {
+    private fun doBackup(arguments: Bundle) {
         val filename = "onion_service" + arguments.getString(OnionServiceActivity.BUNDLE_KEY_PORT) + ".zip"
         if (arguments.getString(OnionServiceActivity.BUNDLE_KEY_DOMAIN) == null) {
-            Toast.makeText(context, R.string.please_restart_Orbot_to_enable_the_changes, Toast.LENGTH_LONG).show()
+            Toast.makeText(requireContext(), R.string.please_restart_Orbot_to_enable_the_changes, Toast.LENGTH_LONG).show()
             return
         }
         val createFileIntent = createWriteFileIntent(filename, "application/zip")
@@ -78,18 +75,16 @@ class OnionServiceActionsDialogFragment internal constructor(arguments: Bundle?)
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_CODE_WRITE_FILE && resultCode == Activity.RESULT_OK) {
-            if (data != null) {
-                attemptToWriteBackup(data.data)
-            }
+            data?.data?.let { attemptToWriteBackup(it) }
         }
     }
 
-    private fun attemptToWriteBackup(outputFile: Uri?) {
+    private fun attemptToWriteBackup(outputFile: Uri) {
         val relativePath = requireArguments().getString(OnionServiceActivity.BUNDLE_KEY_PATH)
-        val v3BackupUtils = V3BackupUtils(context)
+        val v3BackupUtils = V3BackupUtils(requireContext())
         val backup = v3BackupUtils.createV3ZipBackup(relativePath, outputFile)
         val message = if (backup != null) R.string.backup_saved_at_external_storage else R.string.error
-        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+        Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
         dismiss()
     }
 
