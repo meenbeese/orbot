24b96fd92517cee09e4fc70dbed9765798cd5bdc
Kuzey Bilgin <kuzeybilgin@proton.me>
2026-09-04 17:49:01 -0400

    Reworking of MoreFragment with Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,7 @@ import java.util.Properties
 plugins {
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.android.application)
+    alias(libs.plugins.compose.compiler)
 }
 
 kotlin {
@@ -59,6 +60,7 @@ configure<ApplicationExtension> {
 
     buildFeatures {
         buildConfig = true
+        compose = true
         viewBinding = true
     }
 
@@ -176,6 +178,17 @@ androidComponents {
 }
 
 dependencies {
+    val composeBom = platform(libs.androidx.compose.bom)
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+
+    implementation(libs.androidx.compose.animation)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+
     implementation(libs.androidx.core)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
