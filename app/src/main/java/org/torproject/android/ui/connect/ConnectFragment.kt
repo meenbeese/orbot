c2e51d6bb9973e8d3d5c177e68a0cfd085b396bb
Kuzey Bilgin <kuzeybilgin@proton.me>
2026-09-19 19:20:00 -0400

    Remove dead activity code

--- app/src/main/java/org/torproject/android/ui/connect/ConnectFragment.kt
+++ app/src/main/java/org/torproject/android/ui/connect/ConnectFragment.kt
@@ -108,14 +108,6 @@ class ConnectFragment : Fragment(),
             }
         }
 
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewModel.events.collect { event ->
-                when (event) {
-                    is ConnectEvent.StartTorAndVpn -> attemptToStartTor()
-                    is ConnectEvent.RefreshMenuList -> refreshMenuList(requireContext())
-                }
-            }
-        }
         binding.switchConnect.setOnClickListener {
             it.isEnabled = false
             it.alpha = 0.38f
